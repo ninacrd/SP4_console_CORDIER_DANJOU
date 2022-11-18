a5f64342c141235ac8cc06bdfb0571b32d32aea1
Nina Cordier <nina.cordier@epfedu.fr>
2022-11-18 11:23:52 +0100

    jeton créé

--- src/sp4_console_cordier_danjou/SP4_console_CORDIER_DANJOU.java
+++ src/sp4_console_cordier_danjou/SP4_console_CORDIER_DANJOU.java
@@ -15,6 +15,9 @@ public class SP4_console_CORDIER_DANJOU {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Jeton jeton1 = new Jeton (jaune);
+        System.out.println(jeton1);
+
     }
     
 }
