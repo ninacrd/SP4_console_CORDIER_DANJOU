7c549ab1057717614c5a8b5c3f360dd775323ca8
Nina Cordier <nina.cordier@epfedu.fr>
2022-11-18 11:13:16 +0100

    jeton créé

--- src/sp4_console_cordier_danjou/Jeton.java
+++ src/sp4_console_cordier_danjou/Jeton.java
@@ -9,15 +9,28 @@ package sp4_console_cordier_danjou;
  * @author Nina
  */
 public class Jeton {
-    private String couleur; /*ajout de l'attribut couleur associé au jeton*/
+    String couleur; /*ajout de l'attribut couleur associé au jeton*/
     
-    public Jeton (String une_couleur) { /*creation d'un constructeur, il initialise
+    private Jeton (String une_couleur) { /*creation d'un constructeur, il initialise
         l'attribut couleur du jeton avec la valeur passée en paramètre*/
         couleur = une_couleur;
     }
     
-    public String lireCouleur() {
+    private String lireCouleur() {
         return couleur;
     }
     
+    @Override
+        public String toString() { /*methode qui permet de retourner la chaine de 
+        caractere que l'on veut afficher quand l'objet est cree*/
+        if (couleur == "rouge") { /*retourne le caractèe en lien avec la couleur du jeton*/
+            return "R";
+        }
+        else if (couleur == "jaune") {
+            return "J";
+        }
+        else {
+            return "erreur";
+        }
+    }
 }
