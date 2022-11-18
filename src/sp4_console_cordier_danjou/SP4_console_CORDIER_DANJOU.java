a64e3e9c3076b63b9fee7bbf12a575c3c4994dc7
Nina Cordier <nina.cordier@epfedu.fr>
2022-11-18 10:46:48 +0100

    création d'un nouveau projet SP4

---
+++ src/sp4_console_cordier_danjou/SP4_console_CORDIER_DANJOU.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sp4_console_cordier_danjou;
+
+/**
+ *
+ * @author Nina
+ */
+public class SP4_console_CORDIER_DANJOU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
