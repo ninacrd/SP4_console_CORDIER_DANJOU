603b0be1610940ed291e360db701d62bd15c672a
matthieu danjou <matthieu.danjou@epfedu.fr>
2022-11-28 19:48:45 +0100

    avancement jusqu'a attributiocouleuraux joueurs

--- src/sp4_console_cordier_danjou/Partie.java
+++ src/sp4_console_cordier_danjou/Partie.java
@@ -4,20 +4,33 @@
  */
 package sp4_console_cordier_danjou;
 
+import java.util.Random;
+
 /**
  *
  * @author Utilisateur
  */
 public class Partie {
-private Joueur [] listeJoueurs=new Joueur[2];
-private Joueur joueurCourant;
-private PlateauDeJeu plateau;
 
-public void  Partie (Joueur joueur1, Joueur joueur2){
-    listeJoueurs [0]=joueur1 ;
-    listeJoueurs [0]=joueur2 ;
-}
+    private Joueur[] listeJoueurs = new Joueur[2];
+    private Joueur joueurCourant;
+    private PlateauDeJeu plateau;
 
-    
- 
+    public void Partie(Joueur J1, Joueur J2) {
+        listeJoueurs[0] = J1;
+        listeJoueurs[1] = J2;
+    }
+
+    public void attribuerCouleurAuxJoueurs() {
+        Random generateurAleat = new Random();
+        int couleur = generateurAleat.nextInt(1);
+        if (couleur == 1) {
+            listeJoueurs[0].affecterCouleur = "rouge";
+            listeJoueurs[1].affecterCouleur = "jaune";
+        } else {
+            listeJoueurs[1].affecterCouleur = "rouge";
+            listeJoueurs[0].affecterCouleur = "jaune";
+        }
+
+    }
 }
