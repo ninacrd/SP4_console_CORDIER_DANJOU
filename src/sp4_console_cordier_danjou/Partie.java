61e858e578e1c0faa53c809f73e0560e1cc7f8ee
matthieu danjou <matthieu.danjou@epfedu.fr>
2022-11-28 19:23:42 +0100

    debut creation classe partie

---
+++ src/sp4_console_cordier_danjou/Partie.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_cordier_danjou;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Partie {
+private Joueur [] listeJoueurs=new Joueur[2];
+private Joueur joueurCourant;
+private PlateauDeJeu plateau;
+
+public void  Partie (Joueur joueur1, Joueur joueur2){
+    listeJoueurs [0]=joueur1 ;
+    listeJoueurs [0]=joueur2 ;
+}
+
+    
+ 
+}
